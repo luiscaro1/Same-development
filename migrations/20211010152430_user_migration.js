b7bf8e31fff67383735e50eb8551114b405095b7
Luis <luis.caro1@upr.edu>
2021-11-12 18:27:51 -0600

    fixes to docker compose files

--- migrations/20211010152430_user_migration.js
+++ migrations/20211010152430_user_migration.js
@@ -1,14 +1,16 @@
-
-
 exports.up = function (knex) {
   return knex.schema.createTable("User", (table) => {
-    table.uuid("uid").unique().primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table
+      .uuid("uid")
+      .unique()
+      .primary()
+      .defaultTo(knex.raw("gen_random_uuid()"));
     table.string("first_name").notNullable();
     table.string("last_name").notNullable();
     table.string("email").unique().notNullable();
     table.string("password").notNullable();
     table.string("user_name").unique().notNullable();
-    table.boolean("is_active").notNullable();
+    table.boolean("is_active");
     table.timestamps(true, true);
   });
 };
