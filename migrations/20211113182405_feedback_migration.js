c88eb0a4f88e348844d840be9112b547c3bc1fc9
Luis <luis.caro1@upr.edu>
2021-11-30 17:27:56 -0400

    fixed feeback email FK migration

--- migrations/20211113182405_feedback_migration.js
+++ migrations/20211113182405_feedback_migration.js
@@ -1,21 +1,22 @@
-
-
-exports.up = function(knex) {
-    return knex.schema.createTable("Feedback", (table) => {
-        table.uuid("fid").primary().unique().defaultTo(knex.raw('gen_random_uuid()'));
-        table.string("email").references("email").inTable("User");
-        table.string("websitedesign");
-        table.integer("ratedesign").notNullable();
-        table.string("websitefunctionality");
-        table.integer("ratefunctionality").notNullable();
-        table.string("gameavailable");
-        table.integer("rategames").notNullable();
-        table.string("generalinformation");
-        table.integer("rateoverall").notNullable();
-      });
+exports.up = function (knex) {
+  return knex.schema.createTable("Feedback", (table) => {
+    table
+      .uuid("fid")
+      .primary()
+      .unique()
+      .defaultTo(knex.raw("gen_random_uuid()"));
+    table.string("email").notNullable();
+    table.string("websitedesign");
+    table.integer("ratedesign").notNullable();
+    table.string("websitefunctionality");
+    table.integer("ratefunctionality").notNullable();
+    table.string("gameavailable");
+    table.integer("rategames").notNullable();
+    table.string("generalinformation");
+    table.integer("rateoverall").notNullable();
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTableIfExists("Feedback");
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("Feedback");
 };
-
